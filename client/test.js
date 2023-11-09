4f1447ba7919de8c9bfb412030c4221ccca97e38
glen15 <glen15@naver.com>
2023-11-09 16:06:58 +0900

    update test.js

--- client/test.js
+++ client/test.js
@@ -3,14 +3,14 @@ const axios = require('axios');
 const expect = require('chai').expect;
 require('dotenv').config();
 
-describe('EC2 설정을 확인합니다.', () => {
-  it('EC2 연결이 되었다면, 서버의 응답으로 "서버 실행중"를 받아야 합니다.', async () => {
+describe('서버 설정을 확인합니다.', () => {
+  it('서버가 실행중이라면, 서버의 응답으로 "서버 실행중"를 받아야 합니다.', async () => {
     const res = await axios.get(`${process.env.REACT_APP_API_URL}`);
     expect(res.data).to.be.eql('서버 실행중');
   });
 });
 
-describe('S3 설정을 확인합니다.', () => {
+describe('정적 웹페이지 설정을 확인합니다.', () => {
   it('S3에 정적 파일들이 업로드 되었는지 확인합니다.', async () => {
     const result = await axios.get(`${process.env.S3_ADDRESS}`);
     const title = result.data.includes('<!doctype html>');
@@ -19,7 +19,7 @@ describe('S3 설정을 확인합니다.', () => {
 });
 
 describe('데이터 베이스 연결 확인', () => {
-  it('RDS를 배포하고, EC2와 연결했으면, "/status" 요청에 대한 응답이 { isLogin: true, isConnectedToDatabase: true } 이어야 한다', async () => {
+  it('RDS를 배포하고, server의 .env에 연결정보를 입력했다면, "/status" 요청에 대한 응답이 { isLogin: true, isConnectedToDatabase: true } 이어야 한다', async () => {
     const res = await axios
       .post(
         `${process.env.REACT_APP_API_URL}/signin`,
