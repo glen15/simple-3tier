0587c2f76b5e736f926352abd58269ffb8804d3b
glen15 <glen15@naver.com>
2023-11-09 15:48:45 +0900

    update for test

--- client/test.js
+++ client/test.js
@@ -3,21 +3,23 @@ const axios = require('axios');
 const expect = require('chai').expect;
 require('dotenv').config();
 
-describe('http - EC2와 S3 설정을 확인합니다.', () => {
-  it('1. EC2 연결이 되었다면, 서버의 응답으로 "서버 실행중"를 받아야 합니다.', async () => {
+describe('EC2 설정을 확인합니다.', () => {
+  it('EC2 연결이 되었다면, 서버의 응답으로 "서버 실행중"를 받아야 합니다.', async () => {
     const res = await axios.get(`${process.env.REACT_APP_API_URL}`);
     expect(res.data).to.be.eql('서버 실행중');
   });
+});
 
-  it('2. S3에 정적 파일들을 업로드 했다면, "Deployment Practice"가 출력되어야 합니다.', async () => {
+describe('S3 설정을 확인합니다.', () => {
+  it('S3에 정적 파일들이 업로드 되었는지 확인합니다.', async () => {
     const result = await axios.get(`${process.env.S3_ADDRESS}`);
-    const title = result.data.includes('Deployment Practice');
+    const title = result.data.includes('<!doctype html>');
     expect(title).to.be.eql(true);
   });
 });
 
-describe('http - RDS, EC2 연결', () => {
-  it('3. RDS를 배포하고, EC2와 연결했으면, "/status" 요청에 대한 응답이 { isLogin: true, isConnectedToDatabase: true } 이어야 한다', async () => {
+describe('데이터 베이스 연결 확인', () => {
+  it('RDS를 배포하고, EC2와 연결했으면, "/status" 요청에 대한 응답이 { isLogin: true, isConnectedToDatabase: true } 이어야 한다', async () => {
     const res = await axios
       .post(
         `${process.env.REACT_APP_API_URL}/signin`,
--- install.sh
+++ install.sh
@@ -1,7 +1,11 @@
+# 터미널에 하나씩 복사붙여넣기
+
 sudo apt-get update
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 nvm install --lts
-# sudo apt-get install npm -y
